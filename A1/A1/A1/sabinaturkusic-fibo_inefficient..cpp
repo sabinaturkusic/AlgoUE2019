44884e537ff5cc5dc2fd47267ec4ae4e18ce2c6e
Sabina Turkusic <turkusic.sabina@gmail.com>
2019-03-07 18:28:14 +0100

    sabinaturkusic#A1

--- A1/A1/A1/sabinaturkusic-fibo_inefficient..cpp
+++ A1/A1/A1/sabinaturkusic-fibo_inefficient..cpp
@@ -1,12 +1,34 @@
 // A1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+//inefficent  - Laufzeit steigt exponentiell
+//time vor programm in konsole aufrufen und dann nmisst es die runtime
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
-int main()
+main()
 {
-    std::cout << "Hello World!\n"; 
+	int n, c, first = 0, second = 1, next;
+
+	cout << "Enter the number of terms of Fibonacci series you want" << endl;
+	cin >> n;
+
+	cout << "First " << n << " terms of Fibonacci series are :- " << endl;
+
+	for (c = 0; c < n; c++)
+	{
+		if (c <= 1)
+			next = c;
+		else
+		{
+			next = first + second;
+			first = second;
+			second = next;
+		}
+		cout << next << endl;
+	}
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
