f96f72fbae8597ff01059011835ca2e0bfdb94d4
Sabina Turkusic <turkusic.sabina@gmail.com>
2019-03-11 20:06:29 +0100

    sabinaturkusic#A1-efficient

--- A1/efficient/efficient/sabinaturkusic-fibo_efficient..cpp
+++ A1/efficient/efficient/sabinaturkusic-fibo_efficient..cpp
@@ -10,42 +10,122 @@
 
 #include "pch.h"
 #include <iostream>
-#include <chrono>
+#include <algorithm>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main()
+vector<int> vectorResults;
+
+int Fibonacci(int n)
 {
-	int n, t1 = 0, t2 = 1, nextTerm = 0;
+	int t1 = 1;
+	int t2 = 1;
+	int nextTerm = 0;
 
-	cout << "Enter the number of terms: ";
-	cin >> n;
-	cout << "Fibonacci Series: ";
-
-	for (int i = 1; i <= n; ++i)
+	for (int i = 0; i <= n; ++i)
 	{
 		// Prints the first two terms.
 		if (i == 1)
 		{
-			cout << " " << t1;
-			continue;
+			vectorResults.push_back(1);
 		}
 		if (i == 2)
 		{
-			cout << t2 << " ";
+			vectorResults.push_back(2);
 			continue;
 		}
 		nextTerm = t1 + t2;
+		vectorResults.push_back(t1);
 		t1 = t2;
 		t2 = nextTerm;
 
-		cout << nextTerm << " " << endl;
 	}
 	return 0;
-	
+
 }
 
+void printHelp() {
+	cout << "Help:" << endl << "-n: print the nth Fibonacci number"
+		<< endl << "--all: print all Fibonacci numbers up to n" << endl;
+	cout << "Example: $githubusername-fibo_inefficient.$suffix -n [--all] [--help]" << endl;
+}
 
+void calcFibonnaci(int n, bool allPrint) //hier wird mit boolschen operatoren und all alle ausgegeben
+{
+	Fibonacci(n);
+	sort(vectorResults.begin(), vectorResults.end());
+	vectorResults.erase(unique(vectorResults.begin(), vectorResults.end()), vectorResults.end());
 
+	if (allPrint)
+	{
+		cout << "1" << endl;
+
+		for (auto const&c : vectorResults)
+		{
+			cout << c << endl;
+		}
+	}
+	else
+	{
+		int size = vectorResults.size();
+		cout << vectorResults[size - 1] << endl;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc <= 2)
+	{
+		//Warum? 
+		//entweder man gibt -n ohne wert an (ist blödsinn -> daher help)
+		//oder man gibt --help an, dann soll help auch anzeigt werden
+		//oder man gibt tatsächlich nur --all ein, was auch blödsinn wäre. 
+		printHelp();
+		return 0;
+	}
+	if (argc > 4)
+	{
+		printHelp();
+		return 0;
+	}
+	if (argc == 3)
+	{
+		string argv1 = argv[1];
+		if (argv1.compare("-n") == 0)
+		{
+			//nun hole ich mir die Zahl nach dem Parameter --n 
+			int argv2 = stoi(argv[2]);  //Exception handling? 
+			calcFibonnaci(argv2, false);
+		}
+		else
+		{
+			printHelp();
+		}
+	}
+	if (argc == 4)
+	{
+		string argv1 = argv[1];
+		string argv3 = argv[3];
+		if (argv1.compare("-n") == 0)
+		{
+			//nun hole ich mir die Zahl nach dem Parameter --n 
+			int argv2 = stoi(argv[2]); //Exception handling?
+			if (argv3.compare("--all") == 0)
+			{
+				calcFibonnaci(argv2, true);
+			}
+			else
+			{
+				printHelp();
+			}
+		}
+		else
+		{
+			printHelp();
+		}
+	}
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
